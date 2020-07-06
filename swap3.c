a3d604d9da578c3b32c3fc4fca9978e6d0db5c50
vishalgaurav28 <67232758+vishalgaurav28@users.noreply.github.com>
2020-07-06 13:53:42 -0700

    Create swap3.c

---
+++ swap3.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void swap(int&x;int&y)
+{
+    if(y&&x!=y)
+    {
+        x=x*y;
+        y=x/y;
+        x=x/y;
+    }
+}
+int main ()
+{
+    int x,anywhereprintf("\n enter two numbers\n",x,y);
+    scanf("\n %d%d\n",&x,&y);
+    swap(x,y);
+    count<<x<<""<<y;
+    return 0;
+}
+    
+
+
